4f6e2155f49b95215026e8dc59e45bf24f97190e
mtho <michael_t_ho@hotmail.com>
2017-12-30 15:02:58 -0800

    Added sortHeap

--- treePlay.py
+++ treePlay.py
@@ -6,13 +6,15 @@ Created on Thu Dec 21 15:58:20 2017
 """
 import importlib
 import linked_binary_tree
+import math
+
 importlib.reload(linked_binary_tree)
 
 MikeTree=linked_binary_tree.LinkedBinaryTree()
 
 #rootPosition=MikeTree._add_root(4)
 
-aList=[1,54,20,54,21,11,90,-4,-5,23,95,32]
+aList=list(np.random.normal(size=(100,)))
 
 def addToTree(tree,theList,st,pos=None):
     #add elements of theList to tree such that tree is complete
@@ -57,10 +59,52 @@ def addToTree2(tree,theList):
     return
             
         
+def max_heapify(tree,pos):
+    #list_pos=tree.breadthfirst_array()
+    #L=len(list_pos)
+    LeftNode = MikeTree.left(pos)
+    RightNode = MikeTree.right(pos)
+    PosVal=pos.element()
+    if LeftNode is None:
+        return
+    elif RightNode is None:
+        LeftVal=LeftNode.element()
+        if LeftVal>PosVal:
+            tree._replace(pos, LeftVal)
+            tree._replace(LeftNode,PosVal)
+            max_heapify(tree,LeftNode)
+    else:
+        LeftVal=LeftNode.element()
+        RightVal=RightNode.element()
+        if RightVal>=LeftVal and RightVal>PosVal:
+            tree._replace(pos,RightVal)
+            tree._replace(RightNode,PosVal)
+            max_heapify(tree,RightNode)
+        elif LeftVal>=RightVal and LeftVal>PosVal:
+            tree._replace(pos,LeftVal)
+            tree._replace(LeftNode,PosVal)
+            max_heapify(tree,LeftNode)
     
-    
-    
-  
-    
+def build_heap(tree):
+        ListPos=tree.breadthfirst_array()
+        L=len(ListPos)
+        if L==1 or L==0:
+            return
+        L2=math.floor(L/2.0)
+        for pos in ListPos[L2-1::-1]:
+            max_heapify(tree,pos)
+
+def sortHeap(tree):
+    build_heap(tree)
+    L=MikeTree._size
+    ListPos=tree.breadthfirst_array()
+    L=len(ListPos)
+    sortedVal=[]
+    for pos in ListPos[L-1:0:-1]:
+        sortedVal.append(MikeTree.root().element())
+        tree._replace(MikeTree.root(), pos.element())
+        MikeTree._delete(pos)
+        max_heapify(MikeTree,MikeTree.root())
+    return sortedVal    
     
         
